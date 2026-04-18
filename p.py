ecaf88a136c36d498f4d5d6c90d376e6ff216565
Philipp Lindner Media & Network <post@philipp-lindner.de>
2026-04-18 22:52:34 +0200

    Update p.py

--- p.py
+++ p.py
@@ -5,7 +5,7 @@ key = b'G7cGxyUt7iaqtz_PRTurZGv3w0KDO83KET5mxfMcSPs='
 cipher_suite = Fernet(key)
 
 # Dein Passwort als Text
-password = b"jOAkER,-,77LC"
+password = b"xxxxxxxxx"
 encrypted_text = cipher_suite.encrypt(password)
 
 print(f"Verschlüsseltes Passwort für die config.json:\n{encrypted_text.decode()}")
